c2bc0110d54719382d6f69f20aad8c16f69b338b
Wanderson Camargo <wandersonwhcr@gmail.com>
2021-05-01 10:24:33 -0300

    Improve docs

--- src/Finder/Finder.php
+++ src/Finder/Finder.php
@@ -4,9 +4,19 @@ declare(strict_types=1);
 
 namespace Griffin\Harpy\Finder;
 
+/**
+ * Finder
+ */
 class Finder
 {
-    public function find(string $filename, string ...$filenames)
+    /**
+     * Find Files
+     *
+     * @param string $filename File
+     * @param string ..$filenames Additional Filenames
+     * @return string[] Found Files
+     */
+    public function find(string $filename, string ...$filenames): array
     {
         array_unshift($filenames, $filename);
 
