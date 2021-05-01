adae987b695e6d8b45570178967c9028bde9f072
Wanderson Camargo <wandersonwhcr@gmail.com>
2021-05-01 10:24:33 -0300

    Initialize VFS

--- tests/units/Finder/FinderTest.php
+++ tests/units/Finder/FinderTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace GriffinTest\Harpy;
 
 use Griffin\Harpy\Finder\Finder;
+use org\bovigo\vfs\vfsStream;
 use PHPUnit\Framework\TestCase;
 
 class FinderTest extends TestCase
@@ -12,32 +13,31 @@ class FinderTest extends TestCase
     protected function setUp(): void
     {
         $this->finder = new Finder();
-    }
 
-    protected function prefix($filename): string
-    {
-        return __DIR__ . '/../../examples/' . $filename;
+        $this->root = vfsStream::setup();
+
+        $this->foo = vfsStream::newFile('Foo.php')->at($this->root);
+        $this->bar = vfsStream::newFile('Bar.php')->at($this->root);
     }
 
     public function testFile(): void
     {
         $filenames = $this->finder->find(
-            $this->prefix('Foo.php'),
-            $this->prefix('Bar.php'),
-            $this->prefix('Unknown.php'),
+            $this->foo->url(),
+            $this->bar->url(),
         );
 
         $this->assertCount(2, $filenames);
-        $this->assertContains($this->prefix('Foo.php'), $filenames);
-        $this->assertContains($this->prefix('Bar.php'), $filenames);
+        $this->assertContains($this->foo->url(), $filenames);
+        $this->assertContains($this->bar->url(), $filenames);
     }
 
     public function testDirectory(): void
     {
-        $filenames = $this->finder->find($this->prefix('.'));
+        $filenames = $this->finder->find($this->root->url());
 
         $this->assertCount(2, $filenames);
-        // $this->assertContains($this->prefix('Foo.php'), $filenames);
-        // $this->assertContains($this->prefix('Bar.php'), $filenames);
+        $this->assertContains($this->foo->url(), $filenames);
+        $this->assertContains($this->bar->url(), $filenames);
     }
 }
